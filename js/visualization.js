557bac85c89b8a569c33122b72d63199c24f5f40
parkeric <park.er@husky.neu.edu>
2020-11-16 16:51:15 -0500

    Updated the index, style, and visualization to include grade breakdown

--- js/visualization.js
+++ js/visualization.js
@@ -11,4 +11,13 @@
       .selectionDispatcher(d3.dispatch(dispatchString))
       ('#vis-svg-1', data);
   })
+  d3.csv("data/cleaned grade break down/Combined grade breakdown.csv").then(data => {
+    const dispatchString = 'selectionUpdated';
+
+    // Create a stacked bar chart given 
+    // a div id selector to put our svg in; and the data to use.
+    let studentBarChart = gradeBreakDown()
+      .selectionDispatcher(d3.dispatch(dispatchString))
+      ('#vis-svg-2', data);
+  })
 })());
