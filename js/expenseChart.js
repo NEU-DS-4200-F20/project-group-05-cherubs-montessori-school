3c2378d2027887fd5a48e2bda067f45cdb3b3510
Srinath Gaddipati <srinath@Srinaths-MacBook-Pro-2.local>
2020-12-09 22:53:10 +0530

    Added more interactivity

--- js/expenseChart.js
+++ js/expenseChart.js
@@ -14,6 +14,7 @@ function expenseChart() {
     // Create the area chart by adding an svg to the div with the id 
     // specified by the selector using the given data
       function chart(selector, data) {
+          console.log(makeOptiondict(data, 2015))
         let newData = createTable(data)
         let svg = d3.select(selector)
         .append('svg')
@@ -78,15 +79,19 @@ function expenseChart() {
         var stackedData = d3.stack()
         .keys(keys)(newData)
 
+        console.log(stackedData, "stacked 000")
+
         svg.selectAll("mylayers")
         .data(stackedData)
         .enter()
         .append("path")
         .attr("fill", function(d) {
+            console.log(z(name), "c000")
             name = d.key;
             return z(name)})
         .attr("d", d3.area()
         .x(function(d,i) { 
+            console.log(d, "checkk 000")
             return x(d.data.year)})
         .y0(function(d) {return y(d[0])})
         .y1(function(d) {return y(d[1])})
@@ -194,6 +199,231 @@ function expenseChart() {
           .style("text-anchor", "end")
           .text(function(d) { return d; });
 
+          let options = [
+          'Printing & Stationery',
+          'Accounting Charges',
+          'School Maintenance',
+          'Property Tax',
+          'Repairs & Maintenances',
+          'Office Maintenance',
+          'Water Charges',
+          'Vehicle Maintenance',
+          'Security Charges',
+          'Festival & Celabration Exp',
+          'Legal Fees',
+          'Depreciation',
+          'Electricity Charges',
+          'Property Tax',
+          'Travelling Exp',
+          'Telephone exp',]
+
+          let filter = d3.select("#Efilter").on("change", change)
+
+          filter
+            .selectAll("option")
+            .data(options)
+            .enter()
+            .append("option")
+            .attr("value", function(d) {return d;})
+            .text(function(d) {return d})
+
+        
+            // document.multiselect('#Efilter');
+
+            // d3.select("#Efilter")
+            // .selectAll("option")
+            // .filter(function (d, i) { 
+            //     return d.selected; 
+            // });
+
+            let newoptions = []
+
+            // let rarea = d3.selectAll("this").selectAll("path")
+
+            function change() {
+                console.log("hey sup")
+                for (var option of d3.select('#Efilter').property("selectedOptions")){
+                    newoptions.push(option.value)
+                }
+
+                console.log(newoptions, "newo")
+                if(newoptions.length > 0) {
+                    console.log(d3.selectAll("path"),"what")
+                    svg.remove();
+
+                    let svg1 = d3.select(selector)
+                    .append('svg')
+                    .attr('preserveAspectRatio', 'xMidYMid meet') 
+                    .attr('width', '100%') 
+                    .style('background-color', '#FFFFFF') 
+                    .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+                
+                    let g = svg1.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    let tooltip = d3.select("div.tooltip2")
+                
+                    //make x axis
+                    let x = d3.scaleBand().domain(data.map(function(d) { return d.Year; })).range([0, width]).padding(1)
+
+                    g.append("g")
+                        .attr("class", "axis axis--x")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(d3.axisBottom(x))
+                
+                    //make y axis
+                    let y = d3.scaleLinear().range([height, 0])
+                    y.domain([0, getnewMax(data)])
+                
+                    g.append("g")
+                        .attr("class", "axis axis--y")
+                        .call(d3.axisLeft(y)
+                        .tickFormat(function(d){
+                            return d;}))
+
+                        
+                
+                    let columnNames = newoptions
+
+                    let color = d3.scaleOrdinal()
+                    .domain(columnNames.concat([]))
+                        .range(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231']);
+                        //    '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff']);
+
+                    // let z = color
+
+                    let z = color
+
+                    let keys = newoptions
+
+                    var stackedData = d3.stack()
+                    .keys(keys)(newData)
+
+                    console.log(newData ,"basic")
+
+                    console.log(stackedData, "stacked 111")
+
+                    svg1.selectAll("mylayers")
+                    .data(stackedData)
+                    .enter()
+                    .append("path")
+                    .attr("fill", function(d) {
+                        console.log(z(name), "c11")
+                        name = d.key;
+                        return z(name)})
+                    .attr("d", d3.area()
+                    .x(function(d,i) { 
+                        console.log(d, "checkk 1")
+                        return x(d.data.year)})
+                    .y0(function(d) {return y(d[0])})
+                    .y1(function(d) {return y(d[1])})
+                    )
+                    .on("click", function (d, i) {
+                        let expenseType = i.key
+                        let years = [2015, 2016, 2017, 2018, 2019]
+                        let result = []
+                        for (let x = 0; x < years.length; x++) {
+                            result.push({
+                                "year": years[x],
+                                "type": expenseType,
+                                "amount": i[x].data[expenseType]
+                            })
+                        }
+                        console.log(result)
+                        console.log(expenseType)
+
+                        d3.select(this).classed('selected')
+                        let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
+                        console.log(dispatchString, "ds")
+                        console.log(g, "this")
+                        dispatcher.call(dispatchString, this, result);
+                    })
+                    .on("mouseover", function (d, i) {
+                        console.log(i, "i")
+                        console.log(d, "d")
+                        tooltip.transition().delay(30).duration(200).style("opacity", 1)
+                        // console.log(d.screenx)
+                        // let xp = x.invert("expense")
+                        // let currentXPosition = d3.mouse(this);
+                        console.log(this, "pos")
+
+                        // console.log(d3.pointer(d, this), "current pos")
+
+                        let xp = d3.pointer(d, this)[0]
+                        let yp = d3.pointer(d, this)[1]
+                        console.log(xp, yp, "coordinates")
+                        if(xp < 240 && xp > 115) {
+                            tooltip.html("amount : " + i[0].data[i.key] + '<br/>' + "year : 2015" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                        }
+                        else if(xp > 240 && xp < 360) {
+                            tooltip.html("amount : " + i[1].data[i.key] + '<br/>' + "year : 2016" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                        }
+                        else if(xp > 360 && xp < 480) {
+                            tooltip.html("amount : " + i[2].data[i.key] + '<br/>' + "year : 2017" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                        }
+                        else if(xp > 480 && xp < 600) {
+                            tooltip.html("amount : " + i[3].data[i.key] + '<br/>' + "year : 2018" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                        }
+                    })
+                    .on("mouseout", function (d) {
+                        tooltip.transition().duration(100).style("opacity", 0)
+                        // return tooltip.style("visibility", "hidden");
+                    })
+                    
+                    
+                    // Appending the Y axis label for the bar chart.
+                    svg1.append("text")
+                    .attr("x", -((height / 2) + margin.top))
+                    .attr("y", (margin.top / 3) - 40) 
+                    .attr("transform", "rotate(-90)")
+                    .attr("text-anchor" ,"middle")
+                    .text("Amount in Indian Rupees")
+                
+                    //Appending the X axis label for the bar chart.
+                    svg1.append("text")
+                    .attr("x", width / 2 + margin.top)
+                    .attr("y", margin.bottom)
+                    .attr("text-anchor" ,"middle")
+                    .text("Expenditure per year by Category")
+
+                    svg1.append("text")
+                    .attr("x", width / 2 + margin.top)
+                    .attr("y", height + 100)
+                    .attr("text-anchor" ,"middle")
+                    .text("Academic Year")
+                
+                    var legendClassArray = [];
+                
+                    var legend = svg1.selectAll(".legend")
+                    .data(color.domain().slice().reverse())
+                    .enter().append("g")
+                    .attr("class", function (d) {
+                        legendClassArray.push(d.replace(/\s/g, ''));
+                        return "legend";
+                    })
+                    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+                
+                    legendClassArray = legendClassArray.reverse();
+                
+                    legend.append("rect")
+                    .attr("x", width - 18)
+                    .attr("width", 18)
+                    .attr("height", 18)
+                    .style("fill", color)
+                    .attr("id", function (d, i) {
+                        return "id" + d.replace(/\s/g, '');
+                    })
+                
+                    legend.append("text")
+                    .attr("x", width - 24)
+                    .attr("y", 9)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", "end")
+                    .text(function(d) { return d; });
+
+                            }
+                        }
+
+
           return chart
         
         }
@@ -204,25 +434,49 @@ function expenseChart() {
             // let i = 0
             for (i = 0; i < years.length; i ++ ) {
                 let year = years[i]
-                inputData.push({
+                inputData.push( {
                     'year': year,
                     'Printing & Stationery': getAmount(year, data, 'Printing & Stationery'),
                     'Accounting Charges': getAmount(year, data, 'Accounting Charges'),
                     'School Maintenance': getAmount(year, data, 'School Maintenance'),
                     'Property Tax': getAmount(year, data, 'Property Tax'),
-                    'Repairs & Maintenances': getAmount(year, data, 'Repairs & Maintenances')
-                    // 'Office Maintenance': getAmount(year, data, 'Office Maintenance'),
-                    // 'Water Charges': getAmount(year, data, 'Water Charges'),
-                    // 'Vehicle Maintenance': getAmount(year, data, 'Vehicle Maintenance'),
-                    // 'Security Charges': getAmount(year, data, 'Security Charges'),
-                    // 'Festival & Celabration Exp': getAmount(year, data, 'Festival & Celabration Exp'),
-                    // 'Legal Fees': getAmount(year, data, 'Legal Fees'),
-                    // 'Depreciation': getAmount(year, data, 'Depreciation'),
-                    // 'Total Expenditure': getAmount(year, data, 'Total Expenditure')
+                    'Repairs & Maintenances': getAmount(year, data, 'Repairs & Maintenances'),
+                    'Office Maintenance': getAmount(year, data, 'Office Maintenance'),
+                    'Water Charges': getAmount(year, data, 'Water Charges'),
+                    'Vehicle Maintenance': getAmount(year, data, 'Vehicle Maintenance'),
+                    'Security Charges': getAmount(year, data, 'Security Charges'),
+                    'Festival & Celabration Exp': getAmount(year, data, 'Festival & Celabration Exp'),
+                    'Legal Fees': getAmount(year, data, 'Legal Fees'),
+                    'Depreciation': getAmount(year, data, 'Depreciation'),
+                    'Electricity Charges': getAmount(year, data, 'Electricity Charges'),
+                    'Property Tax': getAmount(year, data, 'Property Tax'),
+                    'Travelling Exp': getAmount(year, data, 'Travelling Exp'),
+                    'Telephone exp': getAmount(year, data, 'Telephone exp'),
+
                 })
             }
 
             return inputData
+        }
+
+        function makeOptiondict(data, year) {
+            let options = createOptions(data) 
+            let dict = {'year' : year}
+            for (i in options) {
+                dict[options[i]] =  getAmount(year, data, options[i])
+            }
+
+            return dict
+        }
+
+        function createOptions(data) {
+            let options = []
+            for (i = 0; i < data.length; i ++) {
+                if(data[i].Type == "Expenditure") {
+                    options.push(data[i].Name)
+                }
+            }
+            return options
         }
 
         function getnewMax(data) {
--- js/incomeChart.js
+++ js/incomeChart.js
@@ -15,6 +15,7 @@ function incomeChart() {
     // Create the area chart by adding an svg to the div with the id 
     // specified by the selector using the given data
       function chart(selector, data) {
+        console.log(createOptions(data), "options")
         let newData = createTable(data)
         console.log(newData)
         let newmax = getnewMax(data)
@@ -189,6 +190,202 @@ function incomeChart() {
           .style("text-anchor", "end")
           .text(function(d) { return d; });
 
+          let newoptions = []
+
+          let options = 
+        [
+        "Day care Fees",
+        "Interest",
+        "Montessori Academic Fees",
+        "Summer camp fees",
+        "Sale of Alto Car",
+        "Application Fees",]
+  
+            let filter = d3.select("#Ifilter").on("change", change)
+  
+            filter
+              .selectAll("option")
+              .data(options)
+              .enter()
+              .append("option")
+              .attr("value", function(d) {return d;})
+              .text(function(d) {return d})
+
+        function change() {
+            for (var option of d3.select('#Ifilter').property("selectedOptions")){
+                newoptions.push(option.value)
+            }
+
+            console.log(newoptions)
+
+            if(newoptions.length > 0) {
+                svg.remove();
+
+                let svg1 = d3.select(selector)
+                .append('svg')
+                .attr('preserveAspectRatio', 'xMidYMid meet') 
+                .attr('width', '100%') 
+                .style('background-color', '#FFFFFF') 
+                .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+            
+                let g = svg1.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                let tooltip = d3.select("div.tooltip2")
+            
+                //make x axis
+                let x = d3.scaleBand().domain(data.map(function(d) { return d.Year; })).range([0, width]).padding(1)
+
+                g.append("g")
+                    .attr("class", "axis axis--x")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x))
+            
+                //make y axis
+                let y = d3.scaleLinear().range([height, 0])
+                y.domain([0, newmax])
+            
+                g.append("g")
+                    .attr("class", "axis axis--y")
+                    .call(d3.axisLeft(y)
+                    .tickFormat(function(d){
+                        return d;}))
+
+                    
+                let columnNames = newoptions
+
+                let color = d3.scaleOrdinal()
+                .domain(columnNames)
+                      .range(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231']);
+
+                let z = color
+
+                let keys = newoptions
+
+                var stackedData = d3.stack()
+                .keys(keys)(newData)
+
+                svg1.selectAll("mylayers")
+                .data(stackedData)
+                .enter()
+                .append("path")
+                .attr("fill", function(d) {
+                    name = d.key;
+                    return z(name)})
+                .attr("d", d3.area()
+                .x(function(d,i) { 
+                    return x(d.data.year)})
+                .y0(function(d) {return y(d[0])})
+                .y1(function(d) {return y(d[1])})
+                )
+                .attr("height", function (d) {
+                    return y(d[0]) - y(d[1]);
+                  })
+                .on("click", function (d, i) {
+                let incomeType = i.key
+                let years = [2015, 2016, 2017, 2018, 2019]
+                let result = []
+                for (let x = 0; x < years.length; x++) {
+                    result.push({
+                        "year": years[x],
+                        "type": incomeType,
+                        "amount": i[x].data[incomeType]
+                    })
+                }
+                console.log(result, "income result")
+                console.log(incomeType)
+
+                d3.select(this).classed('selected')
+                let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
+                console.log(dispatchString, "ds")
+                console.log(g, "this")
+                dispatcher.call(dispatchString, this, result);
+                })
+                .on("mouseover", function (d, i) {
+                    console.log(i, "i")
+                    console.log(d, "d")
+                    tooltip.transition().delay(30).duration(200).style("opacity", 1)
+                    // console.log(d.screenx)
+                    // let xp = x.invert("expense")
+                    // let currentXPosition = d3.mouse(this);
+                    console.log(this, "pos")
+
+                    // console.log(d3.pointer(d, this), "current pos")
+
+                    let xp = d3.pointer(d, this)[0]
+                    let yp = d3.pointer(d, this)[1]
+                    console.log(xp, yp, "coordinates")
+                    if(xp < 240 && xp > 115) {
+                        tooltip.html("amount : " + i[0].data[i.key] + '<br/>' + "year : 2015" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                    }
+                    else if(xp > 240 && xp < 360) {
+                        tooltip.html("amount : " + i[1].data[i.key] + '<br/>' + "year : 2016" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                    }
+                    else if(xp > 360 && xp < 480) {
+                        tooltip.html("amount : " + i[2].data[i.key] + '<br/>' + "year : 2017" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                    }
+                    else if(xp > 480 && xp < 600) {
+                        tooltip.html("amount : " + i[3].data[i.key] + '<br/>' + "year : 2018" + '<br/>' + "type : " + i.key).style("left", d.pageX + "px").style("top", d.pageY + "px")
+                    }
+                })
+                .on("mouseout", function (d) {
+                    tooltip.transition().duration(100).style("opacity", 0)
+                    // return tooltip.style("visibility", "hidden");
+                  })
+            
+                
+                // Appending the Y axis label for the bar chart.
+                  svg1.append("text")
+                  .attr("x", -((height / 2) + margin.top))
+                  .attr("y", (margin.top / 3) - 40) 
+                  .attr("transform", "rotate(-90)")
+                  .attr("text-anchor" ,"middle")
+                  .text("Amount in Indian Rupees")
+            
+                //Appending the X axis label for the bar chart.
+                svg1.append("text")
+                  .attr("x", width / 2 + margin.top)
+                  .attr("y", margin.bottom)
+                  .attr("text-anchor" ,"middle")
+                  .text("Income per year by Category")
+
+                svg1.append("text")
+                  .attr("x", width / 2 + margin.top)
+                  .attr("y", height + 100)
+                  .attr("text-anchor" ,"middle")
+                  .text("Academic Year")
+            
+                  var legendClassArray = [];
+            
+                  var legend = svg1.selectAll(".legend")
+                  .data(color.domain().slice().reverse())
+                  .enter().append("g")
+                  .attr("class", function (d) {
+                    legendClassArray.push(d.replace(/\s/g, ''));
+                    return "legend";
+                  })
+                  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+            
+                  legendClassArray = legendClassArray.reverse();
+            
+                  legend.append("rect")
+                  .attr("x", width - 18)
+                  .attr("width", 18)
+                  .attr("height", 18)
+                  .style("fill", color)
+                  .attr("id", function (d, i) {
+                    return "id" + d.replace(/\s/g, '');
+                  })
+            
+                  legend.append("text")
+                  .attr("x", width - 24)
+                  .attr("y", 9)
+                  .attr("dy", ".35em")
+                  .style("text-anchor", "end")
+                  .text(function(d) { return d; });
+            }
+
+        }
+
           return chart
         
         }
@@ -206,10 +403,20 @@ function incomeChart() {
                     'Interest': getAmount(year, data, 'Interest'),
                     'Summer camp fees': getAmount(year, data, 'Summer camp fees'),
                     'Sale of Alto Car': getAmount(year, data, 'Sale of Alto Car'),
-                    'Total Income': getAmount(year, data, 'Total Income')
+                    "Montessori Academic Fees": getAmount(year, data, 'Montessori Academic Fees')
                 })
             }
             return inputData
+        }
+
+        function createOptions(data) {
+            let options = []
+            for (i = 0; i < data.length; i ++) {
+                if(data[i].Type == "Income") {
+                    options.push(data[i].Name)
+                }
+            }
+            return options
         }
 
         function getnewMax(data) {
